b851022df4f10354d104b1ed5eca4863ed9ec128
kumararvind269 <kumararvind269@gmail.com>
2018-03-20 23:21:35 +0530

    1

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into STOCK_TBL (id,name,price)
+values(10001,'A1', 111);
+
+insert into STOCK_TBL (id,name,price)
+values(10002,'A2', 222);
+
+insert into STOCK_TBL (id,name,price)
+values(10003,'A3', 333);
+
+insert into STOCK_TBL (id,name,price)
+values(10004,'A4', 444);
